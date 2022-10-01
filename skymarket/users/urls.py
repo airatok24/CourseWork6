423f495befc09291e61f475e82cde6d1c9697b51
airatok24 <101453810+airatok24@users.noreply.github.com>
2022-10-01 16:29:48 +0300

    Update urls.py

--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -4,5 +4,9 @@ from rest_framework.routers import SimpleRouter
 # TODO подключите UserViewSet из Djoser.views к нашим urls.py
 # TODO для этокого рекоммендуется использовать SimpleRouter
 
+user_router = SimpleRouter()
+user_router.register("users", UserViewSet, basename="users")
+
 urlpatterns = [
+    path("", include(user_router.urls))
 ]
